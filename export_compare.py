0a7e70e4b6b751daa48e7285ba0b01016ff6f801
keighbee <kb@huggingface.co>
2024-10-28 10:39:16 -0700

    added timing checks to the compare file

--- export_compare.py
+++ export_compare.py
@@ -1,9 +1,10 @@
-import torch
-from torchvision.transforms import Compose, Normalize, ToTensor
+import time
 
 import coremltools as ct
 import numpy as np
+import torch
 from PIL import Image
+from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class ResizeCustom:
@@ -15,9 +16,11 @@ class ResizeCustom:
             align_corners=False,
         )
 
+
 class BatchSingle:
     def __call__(self, image):
         return image.unsqueeze(0)
+
 
 transform = Compose(
     [
@@ -36,16 +39,33 @@ image = transform(image_orig)
 pt_model = torch.jit.load("exports/traced_model.pt").eval()
 ct_model = ct.models.MLModel("exports/depthpro.mlpackage")
 
+pt_start_time = time.perf_counter()
 pt_out = pt_model(image)
-ct_out = ct_model.predict({
-    "pixel_values": image
-})
+pt_end_time = time.perf_counter()
+pt_elapsed_time = pt_end_time - pt_start_time
+
+ct_start_time = time.perf_counter()
+ct_out = ct_out = ct_model.predict({"pixel_values": image})
+ct_end_time = time.perf_counter()
+ct_elapsed_time = ct_end_time - ct_start_time
 
 pt_canonical_inverse_depth, pt_fov_deg = pt_out[0].detach().numpy(), pt_out[1].detach().numpy()
 ct_canonical_inverse_depth, ct_fov_deg = ct_out["depth_meters"], ct_out["view_angle"]
 
-diff = np.abs(pt_canonical_inverse_depth-ct_canonical_inverse_depth)
+
+diff = np.abs(pt_canonical_inverse_depth - ct_canonical_inverse_depth)
 print("min", np.min(diff))
 print("median", np.median(diff))
 print("mean", np.mean(diff))
 print("max", np.max(diff))
+
+percent_time_diff = 100 * abs(ct_elapsed_time - pt_elapsed_time) / pt_elapsed_time
+print("pytorch time", pt_elapsed_time, "s")
+print("coreml time", ct_elapsed_time, "s")
+print(
+    "coreml was",
+    percent_time_diff,
+    "%",
+    "faster" if pt_elapsed_time > ct_elapsed_time else "slower",
+    "than pytorch",
+)
